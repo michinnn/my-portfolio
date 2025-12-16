4429944c1d17b50f40013d42ee4e3050920e9df9
Ma. Andrhea Ivhon <maandrheaivhon.work@gmail.com>
2025-12-16 14:52:41 +0800

    dist

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
-const repoName = 'mai-portfolio';
-
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: `/${repoName}/`,
+  base: '/mai-portfolio/',
 })
