1e9ebe3af0455a51fef9fc923570a54343b9b251
Ma. Andrhea Ivhon <maandrheaivhon.work@gmail.com>
2025-12-16 15:25:07 +0800

    com

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // replace with your repo name if different
   base: '/mai-portfolio/',
   plugins: [react()],
 });
